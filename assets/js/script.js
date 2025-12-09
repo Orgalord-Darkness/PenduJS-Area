d606d54f0097fbf762e54f15976d14a49edfa713
Orgalord-Darkness <144434367+Orgalord-Darkness@users.noreply.github.com>
2025-12-09 21:26:44 +0100

    reset script.js with new logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,69 @@
-import {words} from "./list.js";
+import {words} from './list.js';
 
-const randomInt = max => Math.floor(Math.random()*max); 
+const randomInt = max => Math.floor(Math.random() * max)
+const container = document.querySelectorAll('container');
 function randWord(tabs){
     const count = tabs.length;
     const n = randomInt(count);
     return tabs[n]; 
 }
+const word = randWord(words); 
 
-
-// const method = () => {
-//     new Promise((resolve) => {
-//         setTimeout(() => {
-//             resolve(randWord(words)); 
-//             console.log(randWord(words))
-//         },2000)
-//     })
-// }; 
-// method();
-
-// const pendu = () => {
-//     const randomWord = randWord(); 
-//     console.log('Choisissez la difficulté : ');     
-// }
-// pendu(); 
-
-function lire() {
-    const valeur = document.getElementById("champ").value;
-    console.log(valeur);
-}
-
-const pendu = () => {
-    const strings = []; 
-    const word = randWord(words); 
+const penduDisplay = (word) => {
+    const strings = [];  
     strings.push(`<div style="display:flex">`); 
     for(let i = 0; i <word.length ; i++){
         strings.push(`<p style="flex-direction: column">_&nbsp;</p>`); 
     }
     strings.push(`</div>`);
-    return strings.join(""); 
+    return strings.join(" "); 
 }
-pendu(); 
 
-const penduInput = () => {
-    const strings = []; 
-    const word = randWord(words);  
-    for(let i = 0; i <word.length ; i++){
-        strings.push(`_&nbsp;`); 
+function display(value){
+    const container = document.querySelector('.container');
+    const show = `<br>
+    <h1>Insérer une valeur</h1>
+    ${penduDisplay(word)}
+    <input type='text' id='response' value='${value}'/>
+    <button id='valider'>Valider</button>`; 
+    container.innerHTML += show;
+}
+
+function pendu(word, response){
+    const results = [];
+    for(let i = 0; i < response.length ; i++){
+        let verif = false; 
+        for(let j = 0; i < word.length ; i++){
+            if(response[i] === word[j]){
+                verif = true; 
+                results.push(response[i])
+            }
+        }
+        if(verif === false){
+            results.push(null);
+        }
     }
-    return strings.join(""); 
+    return results; 
 }
-const placeholder = () => {
-    return `<input type='text' value='${penduInput()}'>`
-}; 
-placeholder();
 
-const container = document.querySelector('.container');
-container.innerHTML += pendu();
-container.innerHTML += placeholder();
+function placeholder(valeur){
+    let retour = '';
+    for(let i = 0; i < valeur.length ; i++){
+        if(valeur[i] === null){
+            retour += '_'; 
+        }else{
+            retour += valeur[i]; 
+        }
+    }
+    return retour; 
+}
+console.log('here');
+document.getElementById('jouer').addEventListener('click',() =>{
+    display(''); 
 
-console.log('test')
+    document.getElementById('valider').addEventListener('click', () => {
+        const input = document.getElementById('response').value;
+        console.log(input);
+              
+    });
+})
