8c32f8a3a3222ff9142e52158c79b4ca2cc1e865
Orgalord-Darkness <heddy.mameri@gmail.com>
2025-12-20 15:14:39 +0100

    wip - mode de difficulte longueur des mots

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,24 @@ import {words} from './list.js';
 
 const randomInt = max => Math.floor(Math.random() * max)
 const container = document.querySelector('.container');
-function randWord(tabs){
+export function randWordDifficult(tabs, minLength){
+    do {
+        const count = tabs.length;
+        const n = randomInt(count);
+        let word = tabs[n]; 
+    }while(word.length < minLength); 
+
+    return word; 
+    
+}
+export function randWord(tabs){
     const count = tabs.length;
     const n = randomInt(count);
-    return tabs[n]; 
+    return tabs[n];
+    
 }
 export const word = randWord(words); 
+
 
 // const penduDisplay = (word, tab) => {
 //     const strings = [];  
